22aef57a11fd61f9e9e015ff0668599778d4855f
ELmustasje <thomasbarth@live.no>
2026-02-11 17:06:27 +0100

    step 1, 2 done. but cant digest entire video since i run out of memory, stores frames as matix

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("boom")
+import cv2
+print("a")
+
+cap = cv2.VideoCapture('data/debatten.mp4')
+frames = []
+
+count = 0
+if not cap.isOpened():
+    print("Error: Could not open video file.")
+else:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+        count += 1
+        if (count == 10):
+            break
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+for frame in frames:
+    print(frame)
